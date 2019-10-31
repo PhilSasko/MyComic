45636e70f8286fd39bdd8d367b1ebddce6dbc056
PhilSasko <sasko@live.se>
2019-10-30 21:10:37 -0400

    Initial project commit

---
+++ MyComic.Domain/Page.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyComic.Domain
+{
+    public class Page
+    {
+        public int IssueId { get; set; }
+        public int PageNumber { get; set; }
+        public string Description { get; set; }
+    }
+}
