8adea3b80c1e475aefa8e73ff8a192acfdf26149
PhilSasko <sasko@live.se>
2020-05-10 17:25:32 -0400

    Removed outdated test project

--- MyComic.UnitTests/TestComicPageRetriever.cs
+++ MyComic.UnitTests/TestComicPageRetriever.cs
@@ -1,4 +1,4 @@
-﻿using MyComic.Domain.Retrievers;
+﻿using MyComic.PageNavigation;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -11,15 +11,15 @@ namespace MyComic.UnitTests
     [TestFixture]
     public class TestComicPageRetriever
     {
+        // TODO: use one test project per thing
         [Test]
         public void TestRetrieve_WhenPage01_ReturnsCorrectFullPath()
         {
             // Arrange
-            LocalFilePathRetriever filePathRetriever = new LocalFilePathRetriever();
-            ComicPageNameResolver resolver = new ComicPageNameResolver(filePathRetriever);
+            ComicPageNameResolver resolver = new ComicPageNameResolver();
 
             // Act
-            string fullPath = resolver.Resolve(1);
+            string fullPath = resolver.GetComicPageNamneFromComicPageNumber(1);
 
             // Assert
             Assert.That(fullPath, Is.EqualTo(@"C:\Users\sasko\source\repos\MyComic\_images\01.jpg"));
