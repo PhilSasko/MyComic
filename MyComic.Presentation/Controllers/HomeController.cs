96b538211fe68fa6ead955e3ee481c985d5d585c
PhilSasko <sasko@live.se>
2020-05-17 19:20:45 -0400

    Switched from hardcoding to using previously created comic page navigators in HomeController

--- MyComic.Presentation/Controllers/HomeController.cs
+++ MyComic.Presentation/Controllers/HomeController.cs
@@ -12,15 +12,26 @@ namespace MyComic.Presentation.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IComicPageBuilder _comicPageBuilder;
         private readonly IDefaultComicPageRetriever _defaultComicPageRetriever;
+        private readonly INextComicPageNavigator _nextComicPageNavigator;
+        private readonly IPreviousComicPageNavigator _previousComicPageNavigator;
 
         public HomeController
             ( ILogger<HomeController> logger
             , IComicPageBuilder comicPageBuilder
-            , IDefaultComicPageRetriever defaultComicPageRetriever)
+            , IDefaultComicPageRetriever defaultComicPageRetriever
+            , INextComicPageNavigator nextComicPageNavigator
+            , IPreviousComicPageNavigator previousComicPageNavigator)
         {
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            _comicPageBuilder = comicPageBuilder ?? throw new ArgumentNullException(nameof(comicPageBuilder));
-            _defaultComicPageRetriever = defaultComicPageRetriever ?? throw new ArgumentNullException(nameof(defaultComicPageRetriever));
+            _logger = logger 
+                ?? throw new ArgumentNullException(nameof(logger));
+            _comicPageBuilder = comicPageBuilder 
+                ?? throw new ArgumentNullException(nameof(comicPageBuilder));
+            _defaultComicPageRetriever = defaultComicPageRetriever 
+                ?? throw new ArgumentNullException(nameof(defaultComicPageRetriever));
+            _nextComicPageNavigator = nextComicPageNavigator 
+                ?? throw new ArgumentNullException(nameof(nextComicPageNavigator));
+            _previousComicPageNavigator = previousComicPageNavigator 
+                ?? throw new ArgumentNullException(nameof(previousComicPageNavigator));
         }
 
         public IActionResult Index(int id = 0)
@@ -39,14 +50,18 @@ namespace MyComic.Presentation.Controllers
 
         public IActionResult NextPage(int pageNumber)
         {
-            pageNumber++;
-            return RedirectToAction("Index", new { Id = pageNumber });
+            ComicPage currentComicPage = _comicPageBuilder.BuildeComicPageFromPageNumber(pageNumber);
+            ComicPage nextComicPage = _nextComicPageNavigator.GetNextComicPage(currentComicPage);
+
+            return RedirectToAction("Index", new { Id = nextComicPage.PageNumber });
         }
 
         public IActionResult PreviousPage(int pageNumber)
         {
-            pageNumber--;
-            return RedirectToAction("Index", new { Id = pageNumber });
+            ComicPage currentComicPage = _comicPageBuilder.BuildeComicPageFromPageNumber(pageNumber);
+            ComicPage previousComicPage = _previousComicPageNavigator.GetPreviousComicPage(currentComicPage);
+
+            return RedirectToAction("Index", new { Id = previousComicPage.PageNumber });
         }
     }
 }
