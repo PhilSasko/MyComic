5548f76b9909e3ae362eb4593d384f25b968519d
Phil <sasko@live.se>
2021-03-01 16:37:40 -0500

    Created MyComic.Utilities and extension method TryOffsetBy<TEntity>. Added tests for TryOffsetBy<TEntity>.

---
+++ MyComic.Utilities/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyComic.Utilities
+{
+    public static class EnumerableExtensions
+    {
+        public static bool TryOffsetBy<TEntity>(
+            this IEnumerable<TEntity> entities, 
+            Func<TEntity, int> idSelector, 
+            int startId, 
+            int idOffset, 
+            out TEntity offsetEntity) where TEntity : class
+        {
+            offsetEntity = entities.SingleOrDefault(entity => idSelector(entity) == startId + idOffset);
+            return !(offsetEntity is null);
+        }
+    }
+}
